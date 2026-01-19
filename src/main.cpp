008d8570359750ad99c2f7b37d189b3080334489
Sahil Samantaray <sahil13399@gmail.com>
2026-01-19 00:40:04 -0500

    Implmented send function

--- src/main.cpp
+++ src/main.cpp
@@ -53,9 +53,14 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   
+  const char *http_response = "HTTP/1.1 200 OK\r\n\r\n";
+  
+  char buffer[1024];
+  recv(client_fd, buffer, sizeof(buffer),0 );
+  send(client_fd, http_response, strlen(http_response), 0);
   close(server_fd);
 
   return 0;
