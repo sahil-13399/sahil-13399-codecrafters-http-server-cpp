37dd3a9b127d60cf509ab4c9b23beb26a35c7186
Sahil Samantaray <sahil13399@gmail.com>
2026-01-20 22:56:37 -0500

    Fixed interleave bug in persistent connections

--- src/main.cpp
+++ src/main.cpp
@@ -12,82 +12,6 @@
 #include <vector>
 #include <thread>
 
-
-std::vector<std::string> split(const std::string& s, char delimiter);
-
-void HandleSingleRequest(int client_fd, std::string directory, std::string http_request)
-{
-  const char *http_response = "HTTP/1.1 200 OK\r\n\r\n";
-  const char *http_reject = "HTTP/1.1 404 Not Found\r\n\r\n";
-  std::cout << "Client connected\n";
-  std::vector<std::string> split_request = split(http_request, ' ');
-  if (split_request[1] == "/")
-  {
-    send(client_fd, http_response, strlen(http_response), 0);
-  }
-  else if (split_request[1].substr(0, 6) == "/echo/")
-  {
-    // ECHO MESSAGE BACK in RESPONSE BODY
-    int len = split_request[1].length() - 6;
-    std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
-    message += std::to_string(len) + "\r\n\r\n" + split_request[1].substr(6, len);
-    send(client_fd, message.c_str(), message.length(), 0);
-  }
-  else if (split_request[1].substr(0, 11) == "/user-agent")
-  {
-    int user_agent_index = http_request.find("User-Agent: ");
-    int end_index = http_request.find("\r\n", user_agent_index);
-    std::string body = http_request.substr(user_agent_index + strlen("User-Agent: "), end_index);
-    std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(body.length() - 4) + "\r\n\r\n" + body;
-    send(client_fd, message.c_str(), message.length(), 0);
-  }
-  else if (split_request[1].substr(0, 7) == "/files/")
-  {
-
-    if (split_request[0] == "POST")
-    {
-      // std::cout<<split_request[split_request.size() - 1]<<" "<<split_request[split_request.size() - 1].length()<<std::endl;
-      int last_index = http_request.rfind("\r\n");
-      std::string body = http_request.substr(last_index + strlen("\r\n"));
-      std::string filename = split_request[1].substr(7);
-      std::string path = directory + filename;
-      std::fstream file(path, std::ios::out);
-      file << body;
-      const char *created_response = "HTTP/1.1 201 Created\r\n\r\n";
-      std::cout << body << std::endl;
-      send(client_fd, created_response, strlen(created_response), 0);
-      file.close();
-    }
-    else
-    {
-      std::string filename = split_request[1].substr(7);
-      std::string path = directory + filename;
-      std::fstream file(path, std::ios::in);
-      if (!file)
-      {
-        std::cerr << "Error opening the file for writing.";
-        send(client_fd, http_reject, strlen(http_reject), 0);
-      }
-      else
-      {
-        std::string content = "";
-        std::string temp;
-        while (std::getline(file, temp))
-        {
-          content += temp;
-        }
-        std::string message = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + std::to_string(content.length()) + "\r\n\r\n" + content;
-        send(client_fd, message.c_str(), message.length(), 0);
-      }
-      file.close();
-    }
-  }
-  else
-  {
-    send(client_fd, http_reject, strlen(http_reject), 0);
-  }
-}
-
 std::vector<std::string> split(const std::string& s, char delimiter) {
     std::vector<std::string> tokens;
     std::string token;
@@ -99,29 +23,77 @@ std::vector<std::string> split(const std::string& s, char delimiter) {
 }
 
 void handle_request(int client_fd, std::string directory) {
-    
-    bool keep_alive = true;
-    while(keep_alive) {
-        char buffer[1024];
-        std::cout << "Client connected\n";
-        ssize_t n = recv(client_fd, buffer, sizeof(buffer), 0);
-        if (n <= 0)
-        {
-          keep_alive = false;
-          return;
+    while(true) {
+      char buffer[1024];
+    const char *http_response = "HTTP/1.1 200 OK\r\n\r\n";
+    const char* http_reject = "HTTP/1.1 404 Not Found\r\n\r\n";    
+    std::cout << "Client connected\n";
+
+        ssize_t n = recv(client_fd, buffer, sizeof(buffer),0 );
+        if(n <= 0) {
+          break;
         }
         buffer[n] = '\0';
         std::string http_request(buffer);
-          if(http_request.find("Connection: close") != std::string::npos) {
-            keep_alive = false;
-          }
-        std::thread(HandleSingleRequest, client_fd, directory, http_request).detach();
+        std::vector<std::string> split_request = split(http_request,' ');
+        if(http_request.find("Connection: close") != std::string::npos) {
+            break;
+        }
+        if(split_request[1] == "/") {
+            send(client_fd, http_response, strlen(http_response), 0);
+        } else if(split_request[1].substr(0, 6) == "/echo/") {
+            //ECHO MESSAGE BACK in RESPONSE BODY
+            int len = split_request[1].length() - 6;
+            std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+            message += std::to_string(len) + "\r\n\r\n" + split_request[1].substr(6, len);
+            send(client_fd, message.c_str(), message.length(), 0);
+        } else if(split_request[1].substr(0, 11) == "/user-agent") {
+            int user_agent_index = http_request.find("User-Agent: ");
+            int end_index = http_request.find("\r\n", user_agent_index);
+            std::string body = http_request.substr(user_agent_index + strlen("User-Agent: "), end_index);
+            std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(body.length() - 4) + "\r\n\r\n" + body;
+            send(client_fd, message.c_str(), message.length(), 0);
+        } else if(split_request[1].substr(0, 7) == "/files/") {
+
+            if(split_request[0] == "POST") {
+              // std::cout<<split_request[split_request.size() - 1]<<" "<<split_request[split_request.size() - 1].length()<<std::endl;
+              int last_index = http_request.rfind("\r\n");
+              std::string body = http_request.substr(last_index + strlen("\r\n"));
+              std::string filename = split_request[1].substr(7);
+              std::string path = directory + filename;
+              std::fstream file(path, std::ios::out);
+              file<<body;
+              const char* created_response = "HTTP/1.1 201 Created\r\n\r\n";
+              std::cout<<body<<std::endl;
+              send(client_fd, created_response, strlen(created_response), 0);
+              file.close();
+            } else {
+              std::string filename = split_request[1].substr(7);
+              std::string path = directory + filename;
+              std::fstream file(path, std::ios::in);
+              if (!file) {
+                  std::cerr << "Error opening the file for writing.";
+                  send(client_fd, http_reject, strlen(http_reject), 0);
+              } else {
+                std::string content = "";
+                std::string temp;
+                while(std::getline(file, temp)) {
+                  content+=temp;
+                }
+                std::string message = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + std::to_string(content.length()) + "\r\n\r\n" + content;
+                send(client_fd, message.c_str(), message.length(), 0);
+              }
+              file.close();
+            }
+            
+        } else {
+            send(client_fd, http_reject, strlen(http_reject), 0);
+        }
     }
     close(client_fd);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
