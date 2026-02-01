1744d5ef2c7786a03ecb51e6840ecce7e681625e
Sahil Samantaray <sahil13399@gmail.com>
2026-02-01 17:24:32 -0500

    Fixed condition_variable wait condition bug

--- src/concurrency/http_thread_pool.cpp
+++ src/concurrency/http_thread_pool.cpp
@@ -2,34 +2,45 @@
 
 
 void HttpThreadPool::enqueue(std::function<void()> func) {
-    //Add the function to queue and notify
-    std::unique_lock<std::mutex> lock(mutex);
-    worker_queue.push(func);
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        worker_queue.push(func);
+    }
     cv.notify_one();
 }
-
 
 void HttpThreadPool::dequeue() {
     while(true) {
         std::function<void()> func;
-        //Waits for new request to be added
+        
         {
             std::unique_lock<std::mutex> lock(mutex);
             cv.wait(lock, [this]{
                 return !worker_queue.empty() || shutdown_flag;
             });
+            
+            if(worker_queue.empty()) {
+                if(shutdown_flag) {
+                    return;
+                }
+                continue;
+            }
+            
             func = worker_queue.front();
             worker_queue.pop();
         }
+        
         func();
     }
 }
 
-
 void HttpThreadPool::shutdown() {
-    std::unique_lock<std::mutex> lock(mutex);
-    shutdown_flag = true;
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        shutdown_flag = true;
+    }
     cv.notify_all();
+    
     for(auto& t : thread_pool) {
         if(t.joinable()) {
             t.join();
